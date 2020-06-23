ed10ef6da582d4fc2c186292c8e9e04b5e22ee31
Kim Su Hyeon <ksh64520@naver.com>
2020-06-24 00:18:33 +0900

    new file

---
+++ practice.py
@@ -0,0 +1,48 @@
+#숫자 자료형
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(5+3)
+print(2*8)
+print((3+3)*1)
+
+# 문자열 자료형
+print('풍선')
+print('나비')
+print("ㅋㅋㅋㅋㅋㅋㅋㅋㅋ")
+print("ㅋ"*9)
+print('ㅋ'*3)
+
+#boolean 자료형
+# 참/ 거짓
+print(5>10) #flase
+print(5<10) #true
+print(True)
+print(False)
+print(not True)
+print(not False)
+print(not(5>10)) #True
+
+#변수
+animal = "인간"
+name = "수현이"
+age = 18
+hobby = "게임"
+is_adult = age >= 20
+
+print(name + "의 종족은 " + animal + "입니다")
+hobby = "코딩"
+print(name + "는 1시간 후에 생일이고, " + hobby + "하는 것을 좋아해요")
+#print(name + "는 성인인가요? : " + str(is_adult))
+print(name, "는 성인인가요? : ", str(is_adult))
+
+#주석
+# 샵주석
+''' 
+여러문장이
+주석처리
+됩니당
+'''
+# ctrl + / = 한 번에 주석 
+# 해제도 같은 방법으로
---
+++ quiz1.py
@@ -0,0 +1,16 @@
+# Quiz) 변수를 이용하여 다음 문장을 출력하시오
+# 변수명 = station
+# 변수값 = 신림, 신도림, 영등포구청, 합정
+# 출력문장 = xx행 열차가 들어오고 있습니다
+
+station = "신림"
+print(station + " 행 열차가 들어오고 있습니다.")
+
+station = "신도림"
+print(station + " 행 열차가 들어오고 있습니다.")
+
+station = "영등포구청"
+print(station + " 행 열차가 들어오고 있습니다.")
+
+station = "합정"
+print(station + " 행 열차가 들어오고 있습니다.")
