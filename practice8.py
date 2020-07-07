c110830c3edfec54bdcd090561f251947d5c7b60
suhyeun <56942468+suhyeun@users.noreply.github.com>
2020-07-07 21:57:40 +0900

    Add files via upload

--- practice8.py
+++ practice8.py
@@ -31,13 +31,33 @@ attack(tank2_name, "1시", tank2_damage)
 
 class Unit:
     def __init__(self, name, hp, damage):
+        # __init__
+        # 파이썬에서 사용되는 생성자
+
+        #self.~ 멤버변수
         self.name = name
         self.hp = hp
         self.damage = damage
         print("{0} 유닛이 생성되었습니다.".format(self.name))
         print("체력 {0}, 공격력 {1}\n".format(self.hp, self.damage))
-    
+
+#marine과 tank는 Unit의 인스턴스
+#객체가 생성될 때는 __init__ 함수에 정의된 갯수와 동일하게 해야함 
 marine1 = Unit("마린", 40, 5)
 marine2 = Unit("마린", 40, 5)
 tank1 = Unit("탱크", 150, 35)
+# marine3 = Unit("마린") #실행불가
+
+#레이스 : 공중 유닛, 비행기, 클로킹(적에게 보이지 않음)
+wraith1 = Unit("레이스", 80, 5)
+print("유닛 이름 : {0}, 공격력 : {1}".format(wraith1.name, wraith1.damage))
+
+# 마인드 컨트롤 : 상대방 유닛을 내 것으로 만드는 것
+wraith2 = Unit("빼앗은 레이스", 80, 5)
+wraith2.clocking = True 
+# 클로킹이라는 변수는 없지만 밖에서 추가로 할당함
+# 단 밖에서 할당한 것은 할당한 객체에만 적용된다.
+
+if wraith2.clocking == True:
+    print("{0}는 현재 클로킹 상태입니다.".format(wraith2.name))
 
